2dcb6c857363c3de7480fc02adecf89e181520e3
Patrik <Muravski.patrik@gmail.com>
2025-05-19 10:37:31 +0300

    Update main.go
    corretction

--- basics/hextobase64/main.go
+++ basics/hextobase64/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 func main() {
 	var hexString string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
