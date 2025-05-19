0fc1d9cbf31793346eb728110d38e317ef4ea063
Patrik <Muravski.patrik@gmail.com>
2025-05-19 17:58:43 +0300

    searching for a key and decrypting
    searching for a key and decrypting it. Many optimizations are possible

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Crypto
 Crypto using Golang/C++
+
+## Notes
+
+### Hex encoding
+every 2 characters represent a byte (e.g., "1b" → 27 in decimal). check asci table
+
+### XOR
+A XOR B = C => C XOR B = A (XOR is reversible).
+
+### Character frequency is a good metric.
+To decrypt english chars - var letter string = "aetoinshr"
---
+++ basics/FindKeyToDecrypt/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+)
+
+// A LOT OF OPTIMIZATION CAN BE DONE
+func main() {
+	// Hex string converted to bytes
+	var hexstr string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	bytes, er := hex.DecodeString(hexstr)
+
+	if er != nil {
+		fmt.Println("Failed to convert")
+	}
+
+	// createad a map to key - decrypted string
+	var Map map[uint8]string = make(map[uint8]string)
+	// string build for efficient string creation
+	var strBuilder strings.Builder
+
+	for key := 0; key <= 255; key++ {
+		decrypted := bytes
+
+		for i, j := range decrypted {
+			decrypted[i] = j ^ byte(key)
+		}
+
+		strBuilder.Write(decrypted)
+		Map[uint8(key)] = strBuilder.String()
+		strBuilder.Reset()
+		key++
+	}
+
+	// can be improved by adding a more key to the map
+	// can be improved by uint8
+	max := 0
+	maxKey := 0 // can be issues with 0
+
+	// iterate through the map
+	for k, v := range Map {
+		var letter string = "aetoinshr"
+		probability := 0
+
+		// count the number of letters in the decrypted string
+		for i := 0; i < len(letter); i++ {
+			probability += strings.Count(v, string(letter[i]))
+		}
+
+		// check if the current key has a higher probability
+		if probability >= max {
+			max = probability
+			if int(k) != maxKey { // check if the current key is not the same as the previous key
+				// delete the previous key from the map
+				deleteFromMap(Map, uint8(maxKey))
+			}
+			maxKey = int(k)
+		} else {
+			delete(Map, k)
+		}
+	}
+
+	// at current state only one key is left in the map
+	for k, v := range Map {
+		fmt.Printf("Key: %d, Decrypted: %s\n", k, v)
+	}
+}
+
+func deleteFromMap(m map[uint8]string, key uint8) {
+	delete(m, key)
+}
