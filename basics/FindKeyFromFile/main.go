8e8182ba6cdc78791ef9f86c6d5dc71d70df32c9
Patrik <Muravski.patrik@gmail.com>
2025-05-21 16:15:40 +0300

    find single key from file works

--- basics/FindKeyFromFile/main.go
+++ basics/FindKeyFromFile/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"os"
+	"strings"
+	"unicode"
 )
 
 const fileName = "data.txt"
@@ -16,44 +19,74 @@ func main() {
 	}
 
 	buffer := bufio.NewScanner(file)
+	defer file.Close()
+
+	bestKey := 0
+	var bestString string
+	bestResult := 0.0
 
 	for {
-		bytes := readFile(buffer)
-		key, bestMatch := searchForBestKey(bytes)
-		if key != 0 {
-			fmt.Printf("Key: %d\n", key)
-			fmt.Printf("Decrypted: %s\n", bestMatch)
+		bytes, end := readFile(buffer)
+		if !end {
+			break
+		}
+		key, bestMatch, probability := searchForBestKey(bytes)
+		Print(key, bestMatch, probability)
+		if probability > bestResult {
+			bestResult = probability
+			bestKey = key
+			bestString = string(bestMatch)
 		}
 	}
 
+	fmt.Printf("Best Key: %d\n", bestKey)
+	fmt.Printf("Best match: %s\n", bestString)
+	fmt.Printf("Length: %d\n", len(bestString))
+	fmt.Printf("Best probability: %f\n", bestResult)
+
 }
 
-func readFile(scanner *bufio.Scanner) []byte {
-	scanner.Scan()
-	bytes := scanner.Bytes()
-
-	return bytes
+func Print(key int, bytes []byte, probability float64) {
+	fmt.Printf("Key: %d\n", key)
+	fmt.Printf("Decrypted: %s\n", string(bytes))
+	fmt.Printf("Probability: %f\n", probability)
 }
 
-func searchForBestKey(bytes []byte) (int, []byte) {
+func readFile(scanner *bufio.Scanner) ([]byte, bool) {
+	end := scanner.Scan()
+	if !end {
+		return nil, false
+	}
+	hexstr := scanner.Text()
+	bytes, er := hex.DecodeString(hexstr)
+	if er != nil {
+		fmt.Println("Failed to convert")
+	}
+
+	return bytes, true
+}
+
+func searchForBestKey(bytes []byte) (int, []byte, float64) {
 	topProbability := 0.0
 	topKey := 0
 	topBytes := make([]byte, len(bytes))
 
 	for key := 0; key <= 255; key++ {
-		copy := bytes
+		// bCopy := bytes does not make a fresh slice—it just makes a new variable pointing at the same underlying array.
+		bCopy := make([]byte, len(bytes))
+		copy(bCopy, bytes)
 
-		copy = xorByKey(copy, byte(key))
-		probability := evaluate(copy)
+		bCopy = xorByKey(bCopy, byte(key))
+		probability := evaluate(bCopy)
 
 		if probability > topProbability {
 			topProbability = probability
 			topKey = key
-			topBytes = copy
+			topBytes = bCopy
 		}
 	}
 
-	return topKey, topBytes
+	return topKey, topBytes, topProbability
 }
 
 func xorByKey(bytes []byte, key byte) []byte {
@@ -65,20 +98,20 @@ func xorByKey(bytes []byte, key byte) []byte {
 }
 
 func evaluate(bytes []byte) float64 {
+	str := string(bytes)
 	frequency := map[rune]float64{
 		'e': 12.7, 't': 9.1, 'a': 8.2, 'o': 7.5, 'i': 7.0, 'n': 6.7,
 		's': 6.3, 'h': 6.1, 'r': 6.0, 'd': 4.3, 'l': 4.0, 'u': 2.8,
 		' ': 13.0,
 	}
 
-	probability := 0.0
-
-	for _, char := range bytes {
-		val, ok := frequency[rune(char)]
-		if ok {
-			probability += val
+	score := 0.0
+	for _, r := range strings.ToLower(str) {
+		if unicode.IsPrint(r) {
+			score += frequency[r]
+		} else {
+			score -= 10
 		}
 	}
-
-	return probability
+	return score
 }
